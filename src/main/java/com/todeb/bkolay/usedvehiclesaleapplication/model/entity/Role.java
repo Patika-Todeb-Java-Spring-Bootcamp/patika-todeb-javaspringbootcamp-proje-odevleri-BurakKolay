55e73778283a6b524c999aa01244f834052baad8
BurakKolay <burakkolay487@gmail.com>
2022-07-30 23:38:58 +0300

    Optimizeted

--- src/main/java/com/todeb/bkolay/usedvehiclesaleapplication/model/entity/Role.java
+++ src/main/java/com/todeb/bkolay/usedvehiclesaleapplication/model/entity/Role.java
@@ -3,7 +3,6 @@ import org.springframework.security.core.GrantedAuthority;
 
 
 public enum Role implements GrantedAuthority {
-
         ROLE_ADMIN, ROLE_CLIENT;
 
         public String getAuthority() {
