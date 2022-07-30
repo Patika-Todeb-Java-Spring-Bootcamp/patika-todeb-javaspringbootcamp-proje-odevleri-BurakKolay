622ba434c513105b9af285ebb874cc82458338e0
BurakKolay <burakkolay487@gmail.com>
2022-07-31 00:27:41 +0300

    Created admin and simple users

---
+++ src/main/java/com/todeb/bkolay/usedvehiclesaleapplication/SampleDataInitiliazer.java
@@ -0,0 +1,35 @@
+package com.todeb.bkolay.usedvehiclesaleapplication;
+
+import com.todeb.bkolay.usedvehiclesaleapplication.model.entity.Role;
+import com.todeb.bkolay.usedvehiclesaleapplication.model.entity.User;
+//import com.todeb.bkolay.usedvehiclesaleapplication.repository.RoleRepository;
+import com.todeb.bkolay.usedvehiclesaleapplication.repository.UserRepository;
+import com.todeb.bkolay.usedvehiclesaleapplication.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class SampleDataInitiliazer implements ApplicationRunner {
+
+    //private final RoleRepository roleRepository;
+    private final UserRepository userRepository;
+    private final UserService userService;
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        // Creating a sample Admin USER
+        User adminUser = new User("admin-user", "adminuser@mail.com", "pass1234");
+        if (!userRepository.existsByUsername(adminUser.getUsername())) {
+            userService.signup(adminUser, true);
+        }
+        // Creating a sample USER
+        User sampleUser = new User("sample-user", "sampleuser@mail.com", "pass1234");
+        if (!userRepository.existsByUsername(sampleUser.getUsername())) {
+            userService.signup(sampleUser, false);
+        }
+
+    }
+}
